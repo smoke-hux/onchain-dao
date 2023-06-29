c8e5315e5b4f6d7b6db6368eea46ba35b5f96e92
smoke-hux <huxleyomondu@live.com>
2023-06-29 04:44:04 +0300

    small

---
+++ hardhat/contracts/CryptoDevsDAO.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+//Interfaces for the fakeMarketplace
+
+interface IFakeNFTMarketplace {
+    // @dev getPrice() returns the purchase price of the NFT from the fakeMarketplace
+    // @return Returns the price in Wei for the NFT
+    function 
+}
+
+contract CryptoDevsDAO is Ownable {
+    // We will write contract code here
+
+    /*
+    now we will need to call fakeMarketplace contract , 
+    */
+}
